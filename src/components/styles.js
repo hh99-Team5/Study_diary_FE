572adbeb34773aa724250b16da49d9cadaf9a6a2
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-03-16 19:01:38 +0900

    [Refactor] 내가 쓴일지 useContext 적용

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
-
-
 export const Wrapper = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-`
+`;
 
 export const Button = styled.div`
     width: 120px;
@@ -21,8 +19,6 @@ export const Button = styled.div`
     cursor: pointer;
 `;
 
-
-
 export const LargeButton = styled.div`
     width: 300px;
     height: 40px;
@@ -33,13 +29,14 @@ export const LargeButton = styled.div`
     justify-content: center;
     align-items: center;
     cursor: pointer;
-`
+`;
+
 export const Icon = styled.div`
     height: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-`
+`;
 
 export const SelectArea = styled.div`
 display: flex;
@@ -48,9 +45,31 @@ align-items: center;
 justify-content: center;
 gap: 10%;
 height: 50%;
-`
+`;
 
 export const Container = styled.div`
 height: 80%;
 width: 70%;
-`
+`;
+
+export const BtnWrapper = styled.div`
+    position: fixed;
+    top: 10%;
+    right: 15%;
+`;
+
+export const MyListButton = styled.div`
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    border: 1px solid black;
+    border-radius: 50%;
+    cursor: pointer;
+    border: none;
+    background-color: tomato;
+    font-weight: 700;
+    font-size: 30px;
+`;
