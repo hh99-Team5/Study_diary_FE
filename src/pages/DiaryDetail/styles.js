216050123e1685e64418c4f5ee5fad1e128554c6
cherryl-dev <155723499+cherryl-dev@users.noreply.github.com>
2024-03-16 19:39:01 +0900

    [Fix] 비회원 상태일 때의 처리 수정
    - 비회원 입장 시, 수정/삭제 버튼, 좋아요 클릭 시 alert창 표시

--- src/pages/DiaryDetail/styles.js
+++ src/pages/DiaryDetail/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
     width: 100%;
-    height: 100%;
+    height: auto;
     position: fixed;
     display: flex;
     flex-direction: column;
